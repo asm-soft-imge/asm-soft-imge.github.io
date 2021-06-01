a14083dfaae89168a9d487833b77c4b23fd267d3
asm-soft-imge <soft-image@hotmail.com>
2021-06-01 11:58:36 +0430

    softimage

---
+++ js/softimage.js
@@ -0,0 +1,45 @@
+jQuery(function () {
+    // Tooltip
+    
+    new jBox('Modal', {
+      attach: '#Modal-3',
+      width: 450,
+      height: 250,
+      closeButton: 'title',
+      animation: false,
+      title: 'AJAX request',
+      ajax: {
+        url: 'https://softimage.ir/dl/softimage.xml',
+        data: {
+          id: '1982',
+          name: 'Stephan Wagner'
+        },
+        method: 'post',
+        reload: 'strict',
+        setContent: false,
+        beforeSend: function () {
+          this.setContent('');
+          this.setTitle(
+            '<div class="ajax-sending">Request  Download Link...</div>'
+          );
+        },
+        complete: function () {
+          this.setTitle('<div class="ajax-complete">Request complete</div>');
+        },
+        success: function (response) {
+          this.setContent(
+            '<div class="ajax-success">Response:<tt>' +
+              JSON.stringify(response) +
+              '</tt></div>'
+          );
+        },
+        error: function () {
+          this.setContent(
+            '<div class="ajax-error">Oops, something went wrong</div>'
+          );
+        }
+      }
+    });
+
+  });
+  
